d6528e91df2a96d828ed3a3dab7cf2220f7dc3a8
d4vp <ldpinchao1@gmail.com>
2023-09-26 16:07:54 -0500

    Crear componente, servicio y modelo (Azure)

---
+++ src/app/azure/azure.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { AzureService } from '../azure.service';
+import { FormBuilder } from '@angular/forms';
+import { AzureModel } from '../azureImagen.model';
+
+@Component({
+  selector: 'app-azure',
+  templateUrl: './azure.component.html',
+  styleUrls: ['./azure.component.css']
+})
+export class AzureComponent {
+
+  selectedFile: File | null = null;
+  imageSelected = false;
+
+  constructor(private azureService: AzureService) {}
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+      this.imageSelected = true;
+    }
+  }
+
+  onSubmit() {
+    if(this.selectedFile) {
+      const formData = new FormData();
+      formData.append('img', this.selectedFile);
+
+      const azureModel = {
+        img: this.selectedFile
+      };
+
+      this.azureService.post(azureModel).subscribe(
+        (response) => {
+          console.log("Exit", this.selectedFile)
+        },
+        (error) => {
+          console.log("Como los dioses", this.selectedFile)
+        }
+      );
+    }
+  }
+
+}
+
+  
+
